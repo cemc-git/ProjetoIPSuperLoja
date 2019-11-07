31e85f61e4cdebe8aac3b3d84bd2bf72e302c1d0
cfpj <cfpj@G3C34.windows.cin.ufpe.br>
2019-11-07 16:43:12 -0300

    Adicionar Funcionário

---
+++ Funcionario/FuncionarioNaoExisteException.java
@@ -0,0 +1,5 @@
+public class FuncionarioNaoExisteException extends Exception {
+	public FuncionarioNaoExisteException() {
+		super("O funcionário não existe");
+	}
+}
---
+++ Funcionario/Pessoa.java
@@ -0,0 +1,12 @@
+
+public abstract class Pessoa {
+private String nome;
+private String cpf;
+private RepositorioVendas historico;
+ 
+
+public Pessoa(String nome, String cpf) {
+this.nome=null;
+this.cpf=null;
+this.historico=null;
+ }
