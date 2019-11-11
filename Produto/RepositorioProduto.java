620dfe50bb59a23b471d1e6d6442e7c08afba688
douglasst2 <55150864+douglasst2@users.noreply.github.com>
2019-11-11 16:51:31 -0300

    classe produto 2

--- Produto/RepositorioProduto.java
+++ Produto/RepositorioProduto.java
@@ -1,6 +1,6 @@
 
 public interface RepositorioProduto {
 		public void cadastrar(Produto produto);
-		public String remover(Produto produto, String nome_Produto);
-		public  boolean procurar(Produto produto, String nome_Produto);
+		public String remover(Produto produto, String nomeproduto);
+		public  boolean procurar(Produto produto, String nomeproduto);
 }
