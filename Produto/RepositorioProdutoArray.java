8e7ab7fe891f341afed77f6fe09a5385d7088c63
douglasst2 <55150864+douglasst2@users.noreply.github.com>
2019-11-11 16:52:05 -0300

    classe produto 2

--- Produto/RepositorioProdutoArray.java
+++ Produto/RepositorioProdutoArray.java
@@ -1,25 +1,36 @@
 
-	public class RepositorioProdutosArray implements RepositorioProduto {
-		private Produto[] produto;
+	
+public class RepositorioProdutosArray implements RepositorioProduto {
+		private Produtos[] produto;
 		private int indice;
-		public RepositorioProdutosArray(int tamanho) {
-			Produto = new Produto[indice];
-			indice++;
+		
+		public RepositorioProdutosArray(int quantidade) {
+			repositorioProdutosArray = new Produtos[quantidade];
+			indice = quantidade;
 		}
+		
 		@Override
 		public void cadastrar(Produto produto) {
-			// TODO Auto-generated method stub
+			for (int i = 0; i<indice; indice; i++) {
+				repositorioProdutosArray[i] = Produto(String nome, double valor, int quantidade, double valordecompra, double valordevenda)
+						
+			}
 			
 		}
 		@Override
-		public String remover(Produto produto, String nome_Produto) {
-			// TODO Auto-generated method stub
-			return null;
+		public String remover(Produto produto, String nomeproduto) {
+			for(int i = 0; i<quantidade; i++) {
+				if(nomeproduto.equals(repositorioProdutosArrays[i].Produto.getNome()))
+					repositorioProdutosArrays[i] = null;
+			return "O Produto " + nomeproduto + " foi removido com sucesso!";
 		}
+			
 		@Override
-		public boolean procurar(Produto produto, String nome_Produto) {
-			// TODO Auto-generated method stub
-			return false;
+		public boolean procurar(Produto produto, String nomeproduto) {
+			for(int i = 0; i<quantidade; i++) {
+				if(nomeproduto.equals(repositorioProdutosArrays[i].Produto.getNome()))
+				break;
+			return true;
 		}
 }
 
