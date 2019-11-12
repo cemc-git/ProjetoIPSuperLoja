e3dd11245a5c6054a3fa6abc679cc98be7445de1
cfpj <cfpj@G3C35.windows.cin.ufpe.br>
2019-11-12 17:12:24 -0300

    Atualizado

--- Funcionario/RepositorioFuncionarioLista.java
+++ Funcionario/RepositorioFuncionarioLista.java
@@ -1,5 +1,6 @@
 
-public class RepositorioFuncionarioLista implements RepositorioFuncionario {
+public class RepositorioFuncionarioLista implements RepositorioFuncionario{
+	
 	private Funcionario funcionario;
 	private RepositorioFuncionarioLista proximo;
 
@@ -23,11 +24,12 @@ public class RepositorioFuncionarioLista implements RepositorioFuncionario {
 	}
 
 	@Override
+	//demetir e igual a remover
 	public void remover(String cpf) throws FuncionarioNaoExisteException {
 	if(this.funcionario.getCpf() !=null) {
-	  if(this.funcionario.getCpf().equals(cpf)) {
+	  if(this.funcionario.getCpf().equals(cpf)) {
 	 this.funcionario=this.proximo.funcionario;
-	 this.proximo=this.proximo.proximo;	    
+	 this.proximo=this.proximo.proximo;	    
 	}else {
 	 this.proximo.remover(cpf);	
 	}	
@@ -37,14 +39,41 @@ public class RepositorioFuncionarioLista implements RepositorioFuncionario {
 	}
 
 	@Override
-	public boolean procurar(String cpf) {
-
-		return false;
+	public void procurar(String cpf) throws FuncionarioNaoExisteException {
+		if (this.getCpf != null) {
+			if (this.getCpf.equals(cpf)) {
+				return this.funcionario;
+			} else {
+				return this.proximo.procurar(cpf);
+			}
+		} else {
+			throw new FuncionarioNaoExisteException();
+		}
 	}
 
 	@Override
-	public void atualizar(Funcionario funcionario) {
+	public void atualizar(String cpf, Funcionario funcionario)throws FuncionarioNaoExisteException {
+	if(this.getCpf != null) {
+	  if(this.getCpf.equals(cpf)) {
+		this.funcionario=funcionario; 
+	  }else {
+		this.proximo.atualizar(cpf, funcionario);  
+	  }
+	}else {
+	throw new FuncionarioNaoExisteException();
+	}
 
+	}
+	public boolean existe(String cpf) {
+	if(this.getCpf != null) {
+	if(this.getCpf.equals(cpf)) {
+	  return true;	
+	}else {
+	return this.proximo.existe(cpf);	
+	}
+	}else {
+	return false;	
+	}
 	}
 
 }
